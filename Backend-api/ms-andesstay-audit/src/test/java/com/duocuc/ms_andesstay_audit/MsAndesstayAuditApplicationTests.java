5304d0e5b8322a2fc4f1d6774740ba28b819d878
SB- Alumno <SB-Alumno@duoc.acad>
2026-09-10 16:56:53 -0300

    Añade MS inciales al Backend

---
+++ Backend-api/ms-andesstay-audit/src/test/java/com/duocuc/ms_andesstay_audit/MsAndesstayAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocuc.ms_andesstay_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAndesstayAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend-api/ms-andesstay-catalog/src/test/java/com/duocuc/ms_andesstay_catalog/MsAndesstayCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocuc.ms_andesstay_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAndesstayCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
